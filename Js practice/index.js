2bcd3ee6bc2f3c2d513a4dce9b39e0b675d54336
unknown <karan.kankatav@springct.com>
2023-02-09 10:21:17 +0530

    Initial commit

---
+++ Js practice/index.js
@@ -0,0 +1,7 @@
+let a = document.getElementsByTagName('div')[0];
+
+
+// a.insertAdjacentHTML("afterbegin","Hello world")
+// a.insertAdjacentHTML("afterend","Hello world")
+// a.insertAdjacentHTML("beforebegin","Hello world")
+// a.insertAdjacentHTML("beforeend","Hello world")
