cc3adb9fd65976d75db80fe8637979a47d31dfe3
unknown <karan.kankatav@springct.com>
2023-02-09 10:36:12 +0530

    delete files

--- Js practice/index.js
+++
@@ -1,7 +0,0 @@
-let a = document.getElementsByTagName('div')[0];
-
-
-// a.insertAdjacentHTML("afterbegin","Hello world")
-// a.insertAdjacentHTML("afterend","Hello world")
-// a.insertAdjacentHTML("beforebegin","Hello world")
-// a.insertAdjacentHTML("beforeend","Hello world")
